8f737c351e6926903986d88d2c9c6952551e729b
Anton Shauchenia <shevchenya.a@gmail.com>
2019-09-25 15:21:36 +0300

    Allow multiple targets

--- main.go
+++ main.go
@@ -12,44 +12,70 @@ import (
 )
 
 const (
-	PORT       = "8081"
-	SCHEME     = "http"
-	SERVER     = "127.0.0.1:3000"
-	SERVER_URL = SCHEME + "://" + SERVER
+	PORT = "8081"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+type Server struct {
+	schema string
+	host   string
+	port   string
+}
+
+func (s Server) URL() string {
+	return s.schema + "://" + s.host + ":" + s.port
+}
+
+// TODO: consider better name
+type Upstream struct {
+	servers []Server
+	idx     int
+	name    string
+}
+
+func (u *Upstream) GetServer() Server {
+	// TODO: find better way for round robin
+	s := u.servers[u.idx%len(u.servers)]
+	u.idx++
+	return s
+}
+
+var servers = []Server{
+	{"http", "127.0.0.1", "3000"},
+	{"http", "127.0.0.1", "4000"},
+}
+
+var upstream = Upstream{servers: servers}
+
+func prepareRequest(r *http.Request) (*http.Request, error) {
 	// TODO: What is context here?
 	// TODO: Should we copy request or we can just change the existing one
 	fwd := r.Clone(r.Context())
+
+	// TODO: consider better name
+	server := upstream.GetServer()
 	// TODO: check this is the way how url should be constructed
-	url, err := url.Parse(SERVER_URL + r.RequestURI)
+	url, err := url.Parse(server.URL() + r.RequestURI)
 	if err != nil {
 		fmt.Println("URL error", err)
-		return
+		// TODO: wrap error and process above
+		return nil, err
 	}
 	fmt.Println("Original url", r.RequestURI, "New url", url)
 	fwd.URL = url
-	fwd.Host = SERVER_URL
+	fwd.Host = server.URL()
 	fwd.RequestURI = ""
+
+	// TODO: Remove/update all required headers
 	// TODO: Set all required headers
 	fwd.Header.Set("X-Forwarded-For", r.RemoteAddr)
-	// TODO: Remove/update all required headers
+	return fwd, nil
+}
 
-	// TODO: timeouts?
-	client := http.Client{}
-
-	resp, err := client.Do(fwd)
-	if err != nil {
-		fmt.Println("Request error", err)
-		return
-	}
-	fmt.Println("Response", resp.StatusCode)
-
+func writeResponse(w http.ResponseWriter, resp *http.Response) error {
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Response body error", err)
-		return
+		return err
 	}
 
 	w.WriteHeader(resp.StatusCode)
@@ -61,20 +87,48 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// TODO: write directly?
-	io.Copy(w, bytes.NewBuffer(body))
+	_, err = io.Copy(w, bytes.NewBuffer(body))
+	if err != nil {
+		fmt.Println("Body write error", err)
+		return err
+	}
+
+	return nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// TODO: return error to user
+
+	fwd, err := prepareRequest(r)
+	if err != nil {
+		// TODO: add logging
+		return
+	}
+
+	// TODO: timeouts?
+	client := http.Client{}
+
+	resp, err := client.Do(fwd)
+	if err != nil {
+		fmt.Println("Request error", err)
+		return
+	}
+	fmt.Println("Response", resp.StatusCode)
+	writeResponse(w, resp)
 }
 
 func main() {
 	// TODO: multiple targets
+	// TODO: redirect rules
 	// TODO: file config
 	// TODO: hot reload
-	// TODO: redirect rules
 	// TODO: graceful shutdown
 	// TODO: signals processing
 	// TODO: logging
 	// TODO: metrics?
 	// TODO: healthchecks?
 	// TODO: targets autodiscovery?
+	// TODO: tests
 
 	// TODO: change HandleFunc?
 	http.HandleFunc("/", handler)
